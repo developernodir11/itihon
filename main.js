771bef28701b38ed90c545450d481a939adb79f1
developernodir11 <usmonovnodir760@gmail.com>
2025-04-09 08:48:13 +0500

    first commit

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 window.addEventListener("scroll", function () {
   toggleBacktop();
 });
+
 let backtop = document.querySelector(".backtop");
 
 function toggleBacktop() {
@@ -21,3 +22,22 @@ function toggleBacktop() {
     backtop.classList.remove("backtop-show");
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("hamburger");
+  const navMenu = document.querySelector(".nav");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
+let modeBtn = document.querySelector(".site_moon");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/sun.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
